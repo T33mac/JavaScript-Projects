ea1485d8fb2cc42c2056b61485700a5ac0e0232e
T33mac <31869841+T33mac@users.noreply.github.com>
2019-10-23 13:06:44 -0700

    Add files via upload
    Add Basic_JavaScipt_Projects folder

---
+++ Basic JavaScript Projects/JS/Basic_JavaScript_1.js
@@ -0,0 +1,14 @@
+{var B = ("\"This is a string!\",said the string");
+
+window.alert(B);}
+
+
+var B = "\"Hey, you're concatenating my string, man!\","
++ "said the concatenate victim.", Cheese = "orange", Face = "15", B = B.fontcolor("green"), Cheese = Cheese.fontcolor("orange");
+
+document.write(B);
+document.write(Cheese);
+document.write(Face);
+document.write(4 + 8);
+document.write(Number(Face) + 13);
+
